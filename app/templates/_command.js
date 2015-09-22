8a0e6c39dd4a683e81cb2f5ef759af21e9eb292a
Sqrt of Saturn <sqrt@octoblu.com>
2015-09-22 10:09:57 -0700

    v6.0.1 added better error handling

--- app/templates/_command.js
+++ app/templates/_command.js
@@ -1,13 +1,22 @@
 'use strict';
 var Connector     = require('./connector');
+var _             = require('lodash');
 var MeshbluConfig = require('meshblu-config');
 var meshbluConfig = new MeshbluConfig({});
 
 var connector = new Connector(meshbluConfig.toJSON());
 
 connector.on('error', function(error) {
+  if(!error){
+    console.error('an unknown error occured');
+    return;
+  }
+  if(_.isPlainObject(error)){
+    console.error(error);
+    return;
+  }
   console.error(error.toString());
-  if(error && error.stack){
+  if(error.stack){
     console.error(error.stack);
   }
 });
